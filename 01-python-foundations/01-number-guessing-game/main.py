286470061a958ee06923e0ac74c8d8845628ce60
Allan <allannoble1990@gmail.com>
2026-09-06 20:45:10 +0100

    Add input validation and replay functionality

--- 01-python-foundations/01-number-guessing-game/main.py
+++ 01-python-foundations/01-number-guessing-game/main.py
@@ -1,36 +1,58 @@
-# Number Guessing Game - Version 1
-# Goal: Build the basic working game before adding additional features.
+# Number Guessing Game - Version 2
+# Goal: Add input validation and replay functionality.
 
-# IMPORTS
 import random
 
-# SETUP
-random_number = random.randint(1, 100)
+play_again = True
 
-player_guesses_made = 0
-player_guesses_remaining = 10
-guessed_correctly = False
+while play_again:
 
-# MAIN GAME LOOP
-while player_guesses_remaining > 0 and not guessed_correctly:
+    random_number = random.randint(1, 100)
 
-    player_guess = input('Guess the number between 1 and 100: ')
-    player_guess = int(player_guess)
-    player_guesses_remaining -= 1
-    player_guesses_made += 1
+    player_guesses_made = 0
+    player_guesses_remaining = 10
+    guessed_correctly = False
 
-    # CHECK THE GUESS
-    if random_number == player_guess:
-        print(f'You guessed the number {random_number} in {player_guesses_made} guesses!')
-        guessed_correctly = True
+    while player_guesses_remaining > 0 and not guessed_correctly:
 
-    elif random_number < player_guess:
-        print(f'{player_guess} is too high!')
-        print(f'You have made {player_guesses_made} and have {player_guesses_remaining} guesses left.')
-    else:
-        print(f'{player_guess} is too low!')
-        print(f'You have made {player_guesses_made} and have {player_guesses_remaining} guesses left.')
+        try:
+            player_guess = int(input('Guess the number between 1 and 100: '))
 
-if not guessed_correctly:
-    print(f'Game over! The number was {random_number}.')
-    print(f'You used all {player_guesses_made} guesses.')
+            if player_guess < 1 or player_guess > 100:
+                print('Please enter a number between 1 and 100.')
+                continue
+
+            player_guesses_remaining -= 1
+            player_guesses_made += 1
+
+        except ValueError:
+            print('That is not a valid number. Please enter a whole number between 1 and 100.')
+            continue
+
+        if random_number == player_guess:
+            print(f'You guessed the number {random_number} in {player_guesses_made} guesses!')
+            guessed_correctly = True
+
+        elif random_number < player_guess:
+            print(f'{player_guess} is too high!')
+            print(f'You have made {player_guesses_made} and have {player_guesses_remaining} guesses left.')
+        else:
+            print(f'{player_guess} is too low!')
+            print(f'You have made {player_guesses_made} and have {player_guesses_remaining} guesses left.')
+
+    if not guessed_correctly:
+        print(f'Game over! The number was {random_number}.')
+        print(f'You used all {player_guesses_made} guesses.')
+
+    while True:
+        answer = input('Would you like to play again? ').strip().lower()
+
+        if answer == 'yes':
+            break
+
+        elif answer == 'no':
+            play_again = False
+            break
+
+        else:
+            print('Please enter yes or no.')
